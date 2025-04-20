5e424ab9f04942462fcf4c8c7bf0789d1eba360b
Lomonosov-Dmitry <cooler7@icloud.com>
2025-04-20 17:35:09 +0300

    Реализован агрегатор, исправлены ошибки

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.controller;
 
 import com.google.protobuf.Empty;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
@@ -28,17 +30,38 @@ public class EventController extends CollectorControllerGrpc.CollectorController
 
     @Override
     public void collectSensorEvent(SensorEventProto event, StreamObserver<Empty> responseObserver) {
-        if (sensorEventHandlers.containsKey(event.getPayloadCase()))
-            sensorEventHandlers.get(event.getPayloadCase()).handle(event);
-        else
-            throw new IllegalArgumentException("Нет обработчика для этого события");
+        try {
+            if (sensorEventHandlers.containsKey(event.getPayloadCase()))
+                sensorEventHandlers.get(event.getPayloadCase()).handle(event);
+            else
+                throw new IllegalArgumentException("Нет обработчика для этого события");
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
     }
 
     @Override
     public void collectHubEvent(HubEventProto event, StreamObserver<Empty> responseObserver) {
-        if (hubEventHandlers.containsKey(event.getPayloadCase()))
-            hubEventHandlers.get(event.getPayloadCase()).handle(event);
-        else
-            throw new IllegalArgumentException("Нет обработчика для этого события");
+        try {
+            if (hubEventHandlers.containsKey(event.getPayloadCase()))
+                hubEventHandlers.get(event.getPayloadCase()).handle(event);
+            else
+                throw new IllegalArgumentException("Нет обработчика для этого события");
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
     }
 }
+
